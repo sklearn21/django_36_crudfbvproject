581a2ec0dd28cec4e989e888cf62547ffd60ee66
sklearn21 <sklearn21@gmail.com>
2021-03-17 09:36:13 +0530

    Populate data with faker module.

--- testapp/admin.py
+++ testapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from testapp.models import Employee
 
 # Register your models here.
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ['eno', 'ename', 'esal', 'eaddr']
+
+
+admin.site.register(Employee, EmployeeAdmin)
