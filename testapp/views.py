171a958720e77601498d7c90918f3ce3dee98595
sklearn21 <sklearn21@gmail.com>
2021-03-17 13:05:13 +0530

    Django_36 complete.

--- testapp/views.py
+++ testapp/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+
+from django.shortcuts import render,redirect
+from testapp.models import Employee
+from testapp.forms import EmployeeForm
 
 # Create your views here.
+
+
+def retrieve_view(request):
+    employees = Employee.objects.all()
+    return render(request, 'testapp/index.html', {'employees': employees})
+
+
+def create_view(request):
+    form = EmployeeForm()
+    if request.method =='POST':
+        form=EmployeeForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    return render(request, 'testapp/create.html', {'form': form})
